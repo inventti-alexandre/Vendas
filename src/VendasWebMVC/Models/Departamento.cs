7b3d1437bd3f39277d610599688accfd9adb374c
Marco Sena <msena2210@gmail.com>
2018-09-21 19:55:01 -0300

    S10AU151-Primeiro controller Departamento e 1o Teste

---
+++ src/VendasWebMVC/Models/Departamento.cs
@@ -0,0 +1,15 @@
+﻿namespace VendasWebMVC.Models
+{
+    public class Departamento
+    {
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get;  set; }
+        public string  Nome { get; set; }
+
+    }
+}
