835aed7a104b3de5dbef43e3d449137ccef00972
Marco Sena <msena2210@gmail.com>
2018-09-21 19:16:42 -0300

    Criação do Projeto

---
+++ src/VendasWebMVC/Models/Entities/Departamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Models.Entities
+{
+    public class Departamento
+    {
+    }
+}
